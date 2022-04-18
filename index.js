80a37116fc664b2fc119f8a080ff7136041ed369
Alison Souza <alison@contasimples.com>
2022-04-18 19:05:25 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const puppeteer = require("puppeteer");
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+
+app.post("/blacklist", async (req, res) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(process.env.ACCESS_URL);
+
+  await page.type("#txt_Username", process.env.USERNAME_ACCESS);
+  await page.type("#txt_Password", process.env.PASSWORD_ACCESS);
+
+  await page.click("#loginbutton");
+
+  await page.waitForNavigation();
+
+  const fullContentFrame = await page.$("iframe[id='menuIframe']");
+  const fullFrame = await fullContentFrame.contentFrame();
+
+  await fullFrame.click("#wifidevCnt");
+
+  const wifiDeviceFrame = await fullFrame.$(
+    "iframe[id='ContectdevmngtPageSrc']"
+  );
+
+  const deviceFrame = await wifiDeviceFrame.contentFrame();
+
+  await deviceFrame.waitForSelector(process.env.BLACKLIST_DEVICE);
+
+  await deviceFrame.click(process.env.BLACKLIST_DEVICE);
+
+  await browser.close();
+
+  return res.status(200).json({
+    message: "sucesso!",
+  });
+});
+
+app.listen(4000, () => {
+  console.log("up!!!");
+});
