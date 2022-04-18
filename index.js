d7b61a9182926448d31855ce8301d4629de05ec4
Alison Souza <alison@contasimples.com>
2022-04-18 19:34:37 -0300

    get add

--- index.js
+++ index.js
@@ -4,39 +4,41 @@ require("dotenv").config();
 
 const app = express();
 
-app.post("/blacklist", async (req, res) => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(process.env.ACCESS_URL);
+app.get("/", (_, res) => res.send("OK"));
 
-  await page.type("#txt_Username", process.env.USERNAME_ACCESS);
-  await page.type("#txt_Password", process.env.PASSWORD_ACCESS);
+// app.post("/blacklist", async (req, res) => {
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
+//   await page.goto(process.env.ACCESS_URL);
 
-  await page.click("#loginbutton");
+//   await page.type("#txt_Username", process.env.USERNAME_ACCESS);
+//   await page.type("#txt_Password", process.env.PASSWORD_ACCESS);
 
-  await page.waitForNavigation();
+//   await page.click("#loginbutton");
 
-  const fullContentFrame = await page.$("iframe[id='menuIframe']");
-  const fullFrame = await fullContentFrame.contentFrame();
+//   await page.waitForNavigation();
 
-  await fullFrame.click("#wifidevCnt");
+//   const fullContentFrame = await page.$("iframe[id='menuIframe']");
+//   const fullFrame = await fullContentFrame.contentFrame();
 
-  const wifiDeviceFrame = await fullFrame.$(
-    "iframe[id='ContectdevmngtPageSrc']"
-  );
+//   await fullFrame.click("#wifidevCnt");
 
-  const deviceFrame = await wifiDeviceFrame.contentFrame();
+//   const wifiDeviceFrame = await fullFrame.$(
+//     "iframe[id='ContectdevmngtPageSrc']"
+//   );
 
-  await deviceFrame.waitForSelector(process.env.BLACKLIST_DEVICE);
+//   const deviceFrame = await wifiDeviceFrame.contentFrame();
 
-  await deviceFrame.click(process.env.BLACKLIST_DEVICE);
+//   await deviceFrame.waitForSelector(process.env.BLACKLIST_DEVICE);
 
-  await browser.close();
+//   await deviceFrame.click(process.env.BLACKLIST_DEVICE);
 
-  return res.status(200).json({
-    message: "sucesso!",
-  });
-});
+//   await browser.close();
+
+//   return res.status(200).json({
+//     message: "sucesso!",
+//   });
+// });
 
 app.listen(process.env.PORT || 4000, () => {
   console.log("up!!!");
