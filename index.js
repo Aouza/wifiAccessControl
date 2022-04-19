53e3ebc4728e20e3fc85235cd47af5b6782b55f9
Alison Souza <alison@contasimples.com>
2022-04-19 01:18:30 -0300

    upzz

--- index.js
+++ index.js
@@ -8,10 +8,34 @@ app.get("/", (_, res) => res.send("OK"));
 
 app.get("/blacklist", async (req, res) => {
   (async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
     const page = await browser.newPage();
-    await page.goto("https://example.com");
-    await page.screenshot({ path: "example.png" });
+    await page.goto(process.env.ACCESS_URL);
+
+    await page.type("#txt_Username", process.env.USERNAME_ACCESS);
+    await page.type("#txt_Password", process.env.PASSWORD_ACCESS);
+
+    await page.click("#loginbutton");
+
+    await page.waitForNavigation();
+
+    const fullContentFrame = await page.$("iframe[id='menuIframe']");
+    const fullFrame = await fullContentFrame.contentFrame();
+
+    await fullFrame.click("#wifidevCnt");
+
+    const wifiDeviceFrame = await fullFrame.$(
+      "iframe[id='ContectdevmngtPageSrc']"
+    );
+
+    const deviceFrame = await wifiDeviceFrame.contentFrame();
+
+    await deviceFrame.waitForSelector(process.env.BLACKLIST_DEVICE);
+
+    await deviceFrame.click(process.env.BLACKLIST_DEVICE);
 
     await browser.close();
   })();
